74ac0062c533798e5194a6197c273dcc3c7fec35
Paloma Dias <106889888+HellenaDiana@users.noreply.github.com>
2022-06-04 22:36:29 +0000

    aula 19

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-fruta = 'Abacate'
-#index 0123456
-print(fruta[3])
 
+nome = 'Marcos'
+sobrenome = 'Silva'
+profissão = 'Programador'
+
+texto = 'O ' + nome + ' ' + sobrenome + ' é um excelente ' + '[' + profissão + ']' 
+
+texto2 = f'O {nome} {sobrenome} é um excelente [{profissão}]'
+
+print(texto)
+print(texto2)
     
+# O Marcos Silva é um excelente [Programador]
